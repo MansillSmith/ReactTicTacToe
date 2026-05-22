49d75f81539064681e81d0dc800b4b256f478808
Mansill Smith <mansill.smith@gmail.com>
2026-05-22 16:55:17 +1200

    initial commit

---
+++ tictactoe/src/App.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+export function Square() {
+    return <button className="square">X</button>;
+}
+
+
+function App() {
+
+    return (
+        <Square/>
+    )
+}
+
+export default App
